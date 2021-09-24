a57f2092a3e2110dbf4903588931f369d5a73938
DESKTOP-VL5REEG\quang <quangnhat180799@gmail.com>
2021-09-24 09:05:46 +0700

    commit Entity in Master

---
+++ src/main/java/com/example/JwtUserService/JwtUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.JwtUserService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtUserServiceApplication.class, args);
+	}
+
+}
